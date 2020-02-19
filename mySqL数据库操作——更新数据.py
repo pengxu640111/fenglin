6033d6a8cae550f0aa951807be14b2eac1ddc6eb
pengxu640111 <61111200+pengxu640111@users.noreply.github.com>
2020-02-19 22:44:06 +0800

    Add files via upload

---
+++ mySqL数据库操作——更新数据.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+#!/usr/bin/python
+import pymysql
+# 打开数据库连接
+db = pymysql.connect("localhost","root","xpP@ssW0rd","wzjh" )
+# 使用cursor()方法获取操作游标
+cursor = db.cursor()
+# SQL 更新语句
+sql = "UPDATE EMPLOYEE SET AGE = AGE + 1 WHERE SEX = '%c'" % ('M')
+try:
+   # 执行SQL语句
+   cursor.execute(sql)
+   # 提交到数据库执行
+   db.commit()
+except:
+   # 发生错误时回滚
+   db.rollback()
+# 关闭数据库连接
+db.close()
